d86de84bc9fd811ea9673e6c0fbce3fa1f52fc8d
Minsung89 <minsung0217@gmail.com>
2019-11-07 22:22:18 +0900

    Time 활용

--- app/src/main/java/com/tjoeum/a20191107_02_datetimepicker/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20191107_02_datetimepicker/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.tjoeum.a20191107_02_datetimepicker
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : BaseActivity() {
 
@@ -21,13 +24,37 @@ class MainActivity : BaseActivity() {
             var datePickerDialog = DatePickerDialog(this,DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                 Toast.makeText(this, "${year}년 ${month+1}월 ${dayOfMonth}일",Toast.LENGTH_LONG).show()
 
-                dateTxt.text = "${year}년 ${month+1}월 ${dayOfMonth}일"
+
+                var selectDate = Calendar.getInstance()
+                selectDate.set(year,month,dayOfMonth)
+
+                var sdf = SimpleDateFormat("yyyy년M월d일")
+                var resultStr = sdf.format(selectDate.time)
+
+
+                dateTxt.text = resultStr
             },2019,11,11)
 
 
             datePickerDialog.show()
         }
 
+        selectTimeBtn.setOnClickListener {
+            var timePickerDialog = TimePickerDialog(this,TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+
+                var seleteTime = Calendar.getInstance()
+                seleteTime.set(Calendar.HOUR_OF_DAY,hourOfDay)
+                seleteTime.set(Calendar.MINUTE, minute)
+
+                var sdf = SimpleDateFormat("a h:m")
+                timeTxt.text = sdf.format(seleteTime.time)
+
+
+            },10,20,true)
+
+            timePickerDialog.show()
+
+        }
 
     }
 
