189a979ea44496b055912962bb21189e53d972e6
Minsung89 <minsung0217@gmail.com>
2019-11-07 22:01:11 +0900

    날짜 선택 창 띄우기 이벤트 처리  - month가 직관적이지 못해서 불편함

--- app/src/main/java/com/tjoeum/a20191107_02_datetimepicker/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20191107_02_datetimepicker/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.tjoeum.a20191107_02_datetimepicker
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setValues()
+        setupEvents()
     }
 
     override fun setupEvents() {
+
+        selectDateBtn.setOnClickListener {
+            var datePickerDialog = DatePickerDialog(this,DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                Toast.makeText(this, "${year}년 ${month+1}월 ${dayOfMonth}일",Toast.LENGTH_LONG).show()
+
+                dateTxt.text = "${year}년 ${month+1}월 ${dayOfMonth}일"
+            },2019,11,11)
+
+
+            datePickerDialog.show()
+        }
+
+
     }
 
     override fun setValues() {
